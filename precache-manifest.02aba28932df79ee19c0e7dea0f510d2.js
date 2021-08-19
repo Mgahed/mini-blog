c77e875f17d70832c03fdc78a07300bb6ee3f4c5
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-08-19 17:47:54 +0200

    adding dist subtree

---
+++ precache-manifest.02aba28932df79ee19c0e7dea0f510d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b8a6e3eb0bc7c19de4",
+    "url": "/mini-blog/css/app.eb65c83d.css"
+  },
+  {
+    "revision": "c60daaf723f5aca0c6bfdffd57207611",
+    "url": "/mini-blog/index.html"
+  },
+  {
+    "revision": "b6b8a6e3eb0bc7c19de4",
+    "url": "/mini-blog/js/app.537856e1.js"
+  },
+  {
+    "revision": "f2ee81ccb40d0c92984c",
+    "url": "/mini-blog/js/chunk-vendors.a459cee1.js"
+  },
+  {
+    "revision": "203310d9f9edd3d8bbffb870d32c71f9",
+    "url": "/mini-blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mini-blog/robots.txt"
+  }
+]);
